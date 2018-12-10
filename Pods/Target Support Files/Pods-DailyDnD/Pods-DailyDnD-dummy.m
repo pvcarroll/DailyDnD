3800203ad040ec0376302e09834d76b70231bd1a
Paul Carroll <p.v.carroll@gmail.com>
2018-12-10 10:48:05 -0600

    alamofire

---
+++ Pods/Target Support Files/Pods-DailyDnD/Pods-DailyDnD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DailyDnD : NSObject
+@end
+@implementation PodsDummy_Pods_DailyDnD
+@end
---
+++ Pods/Target Support Files/Pods-DailyDnDTests/Pods-DailyDnDTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DailyDnDTests : NSObject
+@end
+@implementation PodsDummy_Pods_DailyDnDTests
+@end
